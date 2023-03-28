f23f0ba46b059fd38dc5444ac891981171670a42
Zeno <jkalexho220@gmail.com>
2023-03-27 22:09:10 -0400

    now they're here

--- abilitiesHelpers.c
+++ abilitiesHelpers.c
@@ -1,7 +1,7 @@
 
 int randomAbility(int p = 0) {
 	//trQuestVarSetFromRand("temp", ZENO_ABILITIES + 1, NICK_ABILITIES + 1, true);
-	trQuestVarSetFromRand("temp", 1, 7, true);
+	trQuestVarSetFromRand("temp", 1, 8, true);
 	trQuestVarSetFromRand("temp2", 1, 12 - xGetInt(dPlayerData, xPlayerLives, p), true);
 	if (trQuestVarGet("temp2") < trQuestVarGet("temp")) {
 		trQuestVarSet("temp", trQuestVarGet("temp2"));
